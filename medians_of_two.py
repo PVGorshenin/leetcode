f13b8a549511cfc503704fca495de2cf894b4c8d
pvgorshenin <mr.gorshenya.job@gmail.com>
2020-09-18 19:01:58 +0300

    median of 2: raw

--- medians_of_two.py
+++ medians_of_two.py
@@ -4,8 +4,18 @@ class Array():
     def __init__(self, arr):
         self.arr = arr
         self.left_margin = 0
-        self.right_margin = len(arr)
-        self.in_left = False
+        self.right_margin = len(arr)-1
+        self.center = len(arr) // 2
+
+    def snapshot(self):
+        self.left_margin_ = self.left_margin
+        self.right_margin_ = self.right_margin
+        self.center_ = self.center
+
+    def restore(self):
+        self.left_margin = self.left_margin_
+        self.right_margin = self.right_margin_
+        self.center = self.center_
 
 
 class MedianOfTwo():
@@ -13,34 +23,74 @@ class MedianOfTwo():
     def __init__(self, arr1, arr2):
         self.arr1 = Array(arr1)
         self.arr2 = Array(arr2)
-        self.arr1.center = self._get_center(arr1.arr)
-        self.arr2.center = self._get_center(arr2.arr)
-        self.left_margin = np.NaN
-        self.right_margin = np.NaN
+        # self.left_margin = self.arr2.left_margin
+        # self.right_margin = self.arr2.right_margin
+        #TODO: remove simplification
+        self.median_position = (len(self.arr1.arr) + len(self.arr2.arr)) // 2
 
     def _get_center(self, arr):
-        return np.ceil(len(arr) / 2)
+        shift = (arr.right_margin - arr.left_margin) // 2
+        return arr.left_margin + shift
 
     def _go_left(self, arr):
-        self.right_margin = arr.center
         arr.right_margin = arr.center
         arr.center = self._get_center(arr)
 
     def _go_right(self, arr):
-        self.right_margin = arr.center
         arr.left_margin = arr.center
         arr.center = self._get_center(arr)
 
-    def _init_compare(self):
-        if self.arr2.center > self.arr1.center:
-            self._go_left(self.arr2)
-            arr.is_left = True
+    def _go_left_to_point(self, arr, point):
+        arr.right_margin = point
+        arr.center = self._get_center(arr)
+
+    def _go_right_to_point(self, arr, point):
+        arr.left_margin = point
+        arr.center = self._get_center(arr)
+
+    def _find_position(self, arr, point):
+        '''
+        Finds closet less
+        '''
+        while (arr.right_margin - arr.left_margin) > 1:
+            if arr.center > point:
+                self._go_left(arr)
+            else:
+                self._go_right(arr)
+        if arr.right_margin != point:
+            return arr.left_margin
+        return arr.right_margin
+
+    def _final_logic(self, left_pointer, right_pointer):
+        if (left_pointer < self.median_position) & (right_pointer > self.median_position):
+            shift = self.median_position - self.arr1.left_margin
+            print(f'Median element --> {self.arr2.arr[self.arr2.left_margin+shift]}')
+        elif left_pointer > self.median_position:
+            print(f'Median element --> {self.arr2.arr[self.median_position]}')
         else:
-            self._go_right(self.arr2)
-            arr.is_left = False
+            print(f'Median element --> {self.arr1.arr[self.median_position]}')
 
     def get_median(self):
-        while (self.right_margin - self.left_margin) > 1:
+        while (self.arr1.right_margin - self.arr1.left_margin) > 1:
+            self.arr2.snapshot()
+            arr2_less_equal_el = self._find_position(self.arr2, self.arr1.center)
+            self.arr2.restore()
+            if self.arr1.center + arr2_less_equal_el > self.median_position:
+                self._go_left(self.arr1)
+                self._go_right_to_point(self.arr2, arr2_less_equal_el)
+            elif self.arr1.center + arr2_less_equal_el < self.median_position:
+                self._go_right(self.arr1)
+                self._go_left_to_point(self.arr2, arr2_less_equal_el)
+            else:
+                print(f'Median element --> {self.arr1.center}')
+                return
+        left_pointer = self.arr1.left_margin + self.arr2.left_margin
+        right_pointer = self.arr1.right_margin + self.arr2.right_margin
+        self._final_logic(left_pointer, right_pointer)
+
+
+MedianOfTwo(list(range(10)), list(range(7))).get_median()
+
 
 
 
