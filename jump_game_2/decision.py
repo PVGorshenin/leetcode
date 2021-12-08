470abee04103fa0f10e4c2f426c7a653680cd6df
Pavel Gorshenin <mr.gorshenya.job@gmail.com>
2021-12-08 14:56:35 +0300

    jump game: prettifying

--- jump_game_2/decision.py
+++ jump_game_2/decision.py
@@ -2,25 +2,30 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n_jumps, i = 0, 0
+        n_jumps, i_number = 0, 0
         if len(nums) == 1:
             return 0
-        if i + nums[i] >= len(nums) - 1:
+
+        if i_number + nums[i_number] >= len(nums) - 1:
             return 1
-        while i < len(nums):
-            n_candidates = nums[i]
+
+        while i_number < len(nums):
+            n_candidates = nums[i_number]
             curr_max_2step_jump = 0
-            curr_maxjump_idx = i
+            curr_maxjump_idx = i_number
+
             for offset in range(1, n_candidates+1):
-                curr_jump = offset + nums[i+offset]
+                curr_jump = offset + nums[i_number+offset]
                 if curr_jump > curr_max_2step_jump:
                     curr_max_2step_jump = curr_jump
-                    curr_maxjump_idx = i + offset
+                    curr_maxjump_idx = i_number + offset
+
             n_jumps += 1
-            i = curr_maxjump_idx
-            if i >= len(nums) - 1:
+            i_number = curr_maxjump_idx
+            if i_number >= len(nums) - 1:
                 return n_jumps
-            if i + nums[i] >= len(nums) - 1:
+
+            if i_number + nums[i_number] >= len(nums) - 1:
                 return n_jumps + 1
 
 
--- jump_game_2/test.py
+++ jump_game_2/test.py
@@ -1,13 +1,14 @@
 from pytest import mark
 from decision import Solution
 
-@mark.parametrize('nums, answer', [([2,3,1,1,4], 2),
-                                    ([2,3,0,1,4], 2),
+
+@mark.parametrize('nums, answer', [([2, 3, 1, 1, 4], 2),
+                                   ([2, 3, 0, 1, 4], 2),
                                    ([1, 2], 1),
                                    ([0], 0),
                                    ([1, 2], 1),
-                                   ([3, 2, 4, 2, 6, 7, 8], 2)
-                                         ]
+                                   ([3, 2, 4, 2, 6, 7, 8], 2),
+                                   ([1, 2, 3, ])]
                   )
 def test_jump(nums, answer):
     assert Solution().jump(nums) == answer
