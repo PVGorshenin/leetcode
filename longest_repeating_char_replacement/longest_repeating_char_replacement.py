3d201ef8b4388d6bdb6e9d48798289295c8fdd1c
pvgorshenin <mr.gorshenya.job@gmail.com>
2024-01-17 16:02:43 +0300

    longest repeating char replacement

---
+++ longest_repeating_char_replacement/longest_repeating_char_replacement.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        i_left, i_right = 0, 0
+
+        chars_freq = defaultdict(int)
+        chars_freq[s[0]] = 1
+        most_freq_char = s[0]
+        i_right = 1
+        max_window = 1
+
+        while i_right < len(s):
+            window_size = i_right - i_left + 1
+            curr_char = s[i_right]
+            chars_freq[curr_char] += 1
+            if chars_freq[curr_char] > chars_freq[most_freq_char]:
+                most_freq_char = curr_char
+
+            if window_size - chars_freq[most_freq_char] > k:
+                chars_freq[s[i_left]] -= 1
+                i_left += 1
+            else:
+                max_window = max(max_window, window_size)
+
+            i_right += 1
+
+        return max_window
+
+
+print(Solution().characterReplacement('aaaabb', 2))
+
+
+
+
+
